91335d72122a725f250d3dd1f3d17478ccea6cfd
Harris730 <harrisimran78@gmail.com>
2025-08-28 00:39:39 +0500

    Data cleaning

---
+++ data_cal.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# arr1= np.array([
+#     [1,2,3],
+#     [4,5,6.5]
+#     ]
+#     )
+# # print(arr1.shape)
+# # print(arr1.size)
+# #print(arr1.dtype)
+# print(np.sum(arr1))
+#slicing
---
+++ url.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+alpha=pd.read_csv("my_file.csv",encoding="ISO-8859-1")
+
+print(alpha)
+
+#print(alpha.isnull().sum())
+
+#alpha=alpha.dropna()
+#print(alpha[["Peak","All Time Peak"]])
+
+alpha["Peak"]=pd.to_numeric(alpha["Peak"].str.split("[").str[0])
+alpha["Peak"].fillna(alpha["Peak"].mean(),inplace=True)
+alpha["Peak"] = alpha["Peak"].round().astype(int)
+
+alpha["All Time Peak"]=pd.to_numeric(alpha["All Time Peak"].str.split("[").str[0])
+alpha["All Time Peak"].fillna(alpha["All Time Peak"].mean(),inplace=True)
+alpha["All Time Peak"] = alpha["All Time Peak"].round().astype(int)
+
+alpha["Ref."]=alpha["Ref."].str.split("[").str[1].str.replace("]","")
+alpha["Tour title"]=alpha["Tour title"].str.split("[").str[0].str.replace("]","")
+alpha["Year(s)"]=alpha["Year(s)"].str.replace("â","-")
+
+#print(alpha["Actual gross"])
+#print(alpha)
+#print(alpha)
