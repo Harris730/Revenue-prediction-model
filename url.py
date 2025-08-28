69e52344982f9e59ec7e84d7d2a4c5363a495d8c
Harris730 <harrisimran78@gmail.com>
2025-08-28 14:10:21 +0500

    redundant data removed

--- url.py
+++ url.py
@@ -1,29 +1,37 @@
 import pandas as pd
 
-alpha=pd.read_csv("my_file.csv",encoding="ISO-8859-1")
 
-#print(alpha.isnull().sum())
+def cleanfile():
+    alpha=pd.read_csv("my_file.csv",encoding="ISO-8859-1")
 
-#alpha=alpha.dropna()
-#print(alpha[["Peak","All Time Peak"]])
+    print(alpha.isnull().sum())
 
-# alpha["Peak"]=pd.to_numeric(alpha["Peak"].str.split("[").str[0])
-# alpha["Peak"].fillna(alpha["Peak"].mean(),inplace=True)
-# alpha["Peak"] = alpha["Peak"].round().astype(int)
+    alpha["Peak"]=pd.to_numeric(alpha["Peak"].str.split("[").str[0]) #spilts and return value outside bracket and converts to numeric
+    alpha["Peak"]=alpha["Peak"].fillna(alpha["Peak"].mean()) #fill missing values with mean
+    alpha["Peak"] = alpha["Peak"].round().astype(int) #rounding off mean values
 
-# alpha["All Time Peak"]=pd.to_numeric(alpha["All Time Peak"].str.split("[").str[0])
-# alpha["All Time Peak"].fillna(alpha["All Time Peak"].mean(),inplace=True)
-# alpha["All Time Peak"] = alpha["All Time Peak"].round().astype(int)
+    alpha["All Time Peak"]=pd.to_numeric(alpha["All Time Peak"].str.split("[").str[0])##
+    alpha["All Time Peak"]=alpha["All Time Peak"].fillna(alpha["All Time Peak"].mean())##
+    alpha["All Time Peak"] = alpha["All Time Peak"].round().astype(int) ##
 
-alpha["Ref."]=alpha["Ref."].str.split("[").str[1].str.replace("]","")
-alpha["Tour title"]=alpha["Tour title"].str.split("[").str[0].str.replace("]","")
-alpha["Year(s)"]=alpha["Year(s)"].str.replace("â","-")
+    alpha["Ref."]=alpha["Ref."].str.split("[").str[1].str.replace("]","") #just using value inside brackets
 
-alpha[["Artist","Tour title"]] = alpha[["Artist","Tour title"]].apply(lambda col: col.str.replace(r'[^A-Za-z\s]', '', regex=True))
+    alpha["Actualgross"]=alpha["Actualgross"].str.split("[").str[0].str.replace("]","") #removing index with brackets
+    alpha["Actualgross"] = alpha["Actualgross"].replace('[\\$,]', '', regex=True).astype(float) #removing $ and , from data and convert to float
 
-alpha["Year(s)"] = alpha["Year(s)"].astype(str).str.rjust(12)
+    alpha["Adjustedgross (in 2022 dollars)"] = alpha["Adjustedgross (in 2022 dollars)"].replace('[\\$,]', '', regex=True).astype(float)#removing $ and , from data and convert to float
 
-print(alpha[["Artist","Tour title","Year(s)"]])
-#print(alpha["Actual gross"])
-#print(alpha)
-#print(alpha)
+    alpha[["Artist","Tour title"]] = alpha[["Artist","Tour title"]].apply(lambda col: col.str.replace(r'[^A-Za-z\s]', '', regex=True)) #just using alphabets
+
+    alpha["Average gross"] = alpha["Average gross"].replace('[\\$,]', '', regex=True).astype(float)#removing $ and , from data and convert to float
+    alpha["Year(s)"]=alpha["Year(s)"].str.replace("â","-") # replacing junks with -
+
+    print(alpha)
+    alpha.to_csv("cleaned_data.csv", index=False, encoding="utf-8-sig")
+
+
+if __name__ == "__main__": 
+    cleanfile()
+
+
+
