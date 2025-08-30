47529d78a8afbce2d29fd1a7132165eb8c46b016
Harris730 <harrisimran78@gmail.com>
2025-08-30 23:10:14 +0500

    data-decoding

--- url.py
+++ url.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+#from sklearn.preprocessing import LabelEncoder
 
 def cleanfile():
     alpha=pd.read_csv("my_file.csv",encoding="ISO-8859-1")
@@ -24,14 +24,23 @@ def cleanfile():
     alpha[["Artist","Tour title"]] = alpha[["Artist","Tour title"]].apply(lambda col: col.str.replace(r'[^A-Za-z\s]', '', regex=True)) #just using alphabets
 
     alpha["Average gross"] = alpha["Average gross"].replace('[\\$,]', '', regex=True).astype(float)#removing $ and , from data and convert to float
+    alpha["Averagegross adjusted"]=(alpha["Adjustedgross (in 2022 dollars)"]/alpha["Shows"]).round(0)
+
     alpha["Year(s)"]=alpha["Year(s)"].str.replace("â","-") # replacing junks with -
 
     print(alpha)
     alpha.to_csv("cleaned_data.csv", index=False, encoding="utf-8-sig")
 
 
-if __name__ == "__main__": 
-    cleanfile()
+def encoding():
+  pd.set_option("display.max_columns", None)
+  df_cod=pd.read_csv("cleaned_data.csv")
+  label=pd.get_dummies(df_cod,columns=["Artist"],dtype=int)
 
+  print(label)
+
+
+if __name__ == "__main__": 
+ encoding()
 
 
